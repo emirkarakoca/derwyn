1f6186f683cf6dc93e38a4f7bca519e39b0e770f
emirkarakoca <karakocamustafaemir@gmail.com>
2026-08-22 23:49:14 +0300

    add jsonutil

---
+++ derwyn/utils/jsonutil.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import json
+
+def load_json(path):
+    try:
+        with path.open("r", encoding="utf-8") as file:
+            return json.load(file)
+    except Exception as e:
+        print("Hata: ", e)
+        return {}
+
+def dump_json(path, data):
+    try:
+        with path.open("w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print("Hata: ", e)
+        return False
