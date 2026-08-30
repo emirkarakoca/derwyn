d8a9930143bc77fe6d31357a188c052c139c981d
emirkarakoca <karakocamustafaemir@gmail.com>
2026-08-30 15:31:49 +0300

    fix dump_json return value

--- derwyn/utils/jsonutil.py
+++ derwyn/utils/jsonutil.py
@@ -13,6 +13,7 @@ def dump_json(path, data):
     try:
         with path.open("w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
+            return True
     except Exception as e:
         print("Hata: ", e)
         return False
